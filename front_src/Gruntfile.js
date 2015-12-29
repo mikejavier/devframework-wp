dd62588e97edac1ae2b385c858cf7e6c41999620
mikejavier <contato@michaelsantillan.com>
2015-12-28 21:34:17 -0300

    atualizando workflow com postcss

--- front_src/Gruntfile.js
+++ front_src/Gruntfile.js
@@ -2,33 +2,47 @@
 
 module.exports = function( grunt ) {
 	// Definição dos arquivos js
-	var filesJS = ['bower_components/modernizr/modernizr.js', 'bower_components/owl-carousel/owl-carousel/owl.carousel.js','src/js/**/*'];
+	var filesJS = ['bower_components/modernizr/modernizr.js', 'bower_components/waves/src/js/waves.js','src/js/**/*'];
 	// Definição dos arquivos css
-	var fileCSS = ['bower_components/normalize-css/normalize.css', 'bower_components/owl-carousel/owl-carousel/owl.carousel.css',  'src/css/sprite/sprite.css','src/css/main.css'];
+	var sassconcat = ['bower_components/normalize-css/normalize.css', 'src/css/sprite/sprite.css','src/css/main_sass.css'];
+	var postcssconcat = ['bower_components/normalize-css/normalize.css', 'src/css/sprite/sprite.css','src/css/main_postcss.css'];
 
 	// Load all tasks
 	require('time-grunt')(grunt);
-	require('jit-grunt')(grunt);
-	//require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+	require('jit-grunt')(grunt,{
+		cmq: 'grunt-combine-media-queries',
+	});
 
 	grunt.initConfig({
 		// Watch
 		watch: {		
-			css: {
+			precss: {
 				files: [ 'src/sass/**/*' ],
-				tasks: [ 'sass', 'concat:css', 'postcss' ]
+				tasks: [ 'sass', 'concat:sass' ]
+			},
+
+			postcss: {
+				files: [ 'src/postcss/**/*.css' ],
+				tasks: [ 'postcss', 'concat:postcss' ]
 			},
 
 			js: {
 				files: 'src/js/**/*',
 				tasks: [ 'concat:js' ]
+			},
+
+			jade: {
+				files: 'views/**/*',
+				tasks: [ 'jade' ]
 			}
 		},
 
 		postcss: {
 	      options: {
 	        map: true,
+	        parser: require('postcss-scss'),
 	        processors: [
+	        	require('precss'),
 	        	require('postcss-responsive-type')(),
           		require('autoprefixer')({browsers: ['last 2 versions']}),
         		require('pixrem')({rootValue:10, html:false}),
@@ -36,27 +50,55 @@ module.exports = function( grunt ) {
 	        ]
 	      },
 	      dist: {
-	        src: '../dist/css/styles.combined.min.css',
-	        dest: '../dist/css/styles.combined.min.css'
+	        src: 'src/postcss/main.css',
+	        dest: 'src/css/main_postcss.css'
 	      }
 	    },
 
-		//SASS para CSS
 	    sass: {
 	        options: {
 	            sourceMap: true
 	        },
 	        dist: {
 	            files: {
-	                'src/css/main.css': 'src/sass/main.scss'
+	                'src/css/main_sass.css': 'src/sass/main.scss'
 	            }
+	        }
+	    },
+
+	    jade: {
+	        compile: {
+	            options: {
+	                client: false,
+	                pretty: true
+	            },
+	            // Create a singles pages  
+	            files: [ {
+	              cwd: "views/",
+	              src: "**/*.jade",
+	              dest: "../",
+	              expand: true,
+	              ext: ".html"
+	            } ]
+            	/*
+            	= Create a templates =
+	            files: {
+			      "path/to/dest.html": "path/to/templates/*.jade"
+			    }
+			    */
 	        }
 	    },
 
 		// Concateção dos arquivos CSS e JS
 		concat: {
-			css: {
-				src: fileCSS,
+			sass: {
+				src: sassconcat,
+				
+				dest: '../dist/css/styles.combined.min.css'
+			},
+
+			postcss: {
+				src: postcssconcat,
 				
 				dest: '../dist/css/styles.combined.min.css'
 			},
@@ -129,7 +171,7 @@ module.exports = function( grunt ) {
 
                 // Integrando com a tarefa "watch"
                 watchTask: true,
-                proxy: "localhost:8888/wordpress",
+                proxy: "localhost:8888/devframework",
 
 
                 // Sincronizando os eventos entre os dispositívos
@@ -142,7 +184,7 @@ module.exports = function( grunt ) {
             }
         },
 
-    	sprity: {
+		sprity: {
 			options: {
 			'cssPath': '../images',
 			'processor': 'css',
@@ -178,10 +220,7 @@ module.exports = function( grunt ) {
 
 	// registrando tarefa default
 	grunt.registerTask( 'default', [ 'browserSync', 'watch' ] );
+	grunt.registerTask( 'dist', [ 'uglify:dist', 'cmq', 'cssmin', 'imagemin' ] );
 	grunt.registerTask( 'img', [ 'sprity', 'imagemin' ] );
-	grunt.registerTask( 'sprite', [ 'sprity' ] );
-	grunt.registerTask( 'css', [ 'cssmin' ] );
-	grunt.registerTask( 'mq', [ 'cmq' ] );
 	grunt.registerTask( 'update', [ 'devUpdate' ] );
-	grunt.registerTask( 'dist', [ 'uglify:dist', 'concat:css', 'cmq', 'cssmin', 'imagemin' ] );
 };
